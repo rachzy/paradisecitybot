403fd5c18e06b43dca65e0c16c69e809f09db946
Henrique <rachultimate@gmail.com>
2022-06-02 18:59:17 -0300

    Fixing Index

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,1 @@
-declare const Discord: any;
-declare const client: any;
-declare const config: any;
-declare const mysql: any;
-declare const fs: any;
-declare const MessageEmbed: any;
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-import { Message } from "discord.js";
+import {
+  Channel,
+  ChannelData,
+  Emoji,
+  Message,
+  MessageEvent,
+  RawOverwriteData,
+  ReactionCollectorOptions,
+  ReactionEmoji,
+  ReactionManager,
+  User,
+} from "discord.js";
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
-const mysql = require("mysql");
-const fs = require("fs");
+const config = require("../config.json");
+
 const { MessageEmbed } = require("discord.js");
 client.on("ready", () => {
   client.user.setActivity("discord.gg/fvgzRYjHET");
@@ -14,31 +24,26 @@ client.on("message", (msg: Message) => {
   let canais = ["838590966821683241"];
   let palavras = ["!whitelist", "!mensagem 1", "!mensagem 2"];
   if (
-    !new RegExp(palavras.join("|", "g")).test(msg.content) &&
+    !new RegExp(palavras.join("|, g")).test(msg.content) &&
     canais.includes(msg.channel.id)
   ) {
     msg.delete();
   }
 });
 
-client.on("raw", async (dados) => {
-  if (
-    dados.t !== "MESSAGE_REACTION_ADD" &&
-    dados.t !== "MESSAGE_REACTION_REMOVE"
-  )
-    return;
-  if (dados.d.message_id != "886216752902586389") return;
+client.on("messageReactionAdd", async (dados: ReactionEmoji, user: User) => {
+  if (dados.id != "886216752902586389") return;
   let servidor = client.guilds.cache.get("878747296639115304");
 
   const canal = servidor.channels.cache.find(
-    (ch) => ch.name === `ticket-${dados.d.user_id}`
+    (ch: ChannelData) => ch.name === `ticket-${dados.id}`
   );
-  const membro = servidor.members.cache.get(dados.d.user_id);
-  const membro2 = dados.d.user_id;
+  const membro = servidor.members.cache.get(dados.id);
+  const membro2 = user.id;
 
   let embed = await new MessageEmbed()
     .setColor("#696969")
-    .setTitle(`TICKET Nº${dados.d.user_id}`)
+    .setTitle(`TICKET Nº${dados.id}`)
     .setDescription(
       `<@${membro2}>, Ticket aberto com sucesso!\nPor gentileza, envie o seu problema.`
     )
@@ -46,54 +51,53 @@ client.on("raw", async (dados) => {
       `Enviei uma mensagem para os Administradores, logo entarão aqui para auxiliá-lo`
     );
 
-  if (dados.t === "MESSAGE_REACTION_ADD") {
-    if (dados.d.emoji.name === "📩") {
-      if (canal) return;
-      servidor.channels
-        .create(`ticket-${dados.d.user_id}`, {
-          type: "text",
-          parent: "846504229325766687",
-          permissionOverwrites: [
-            {
-              allow: [
-                "VIEW_CHANNEL",
-                "READ_MESSAGE_HISTORY",
-                "EMBED_LINKS",
-                "ATTACH_FILES",
-                "SEND_MESSAGES",
-              ],
-              id: dados.d.user_id,
-            },
-            {
-              deny: "VIEW_CHANNEL",
-              id: dados.d.guild_id,
-            },
-            {
-              allow: [
-                "VIEW_CHANNEL",
-                "READ_MESSAGE_HISTORY",
-                "EMBED_LINKS",
-                "ATTACH_FILES",
-                "SEND_MESSAGES",
-              ],
-              id: "886216752902586389",
-            },
-          ],
-        })
-        .then(async (msg) => {
-          msg.send(embed);
-          msg.send(`<@${dados.d.user_id}>`).then(async (mencao) => {
-            setTimeout(function () {
-              mencao.delete();
-            }, 1000);
-          });
+  if (dados.name === "📩") {
+    if (canal) return;
+    servidor.channels
+      .create(`ticket-${dados.id}`, {
+        type: "text",
+        parent: "846504229325766687",
+        permissionOverwrites: [
+          {
+            allow: [
+              "VIEW_CHANNEL",
+              "READ_MESSAGE_HISTORY",
+              "EMBED_LINKS",
+              "ATTACH_FILES",
+              "SEND_MESSAGES",
+            ],
+            id: dados.id,
+          },
+          {
+            deny: "VIEW_CHANNEL",
+            id: client.user.id,
+          },
+          {
+            allow: [
+              "VIEW_CHANNEL",
+              "READ_MESSAGE_HISTORY",
+              "EMBED_LINKS",
+              "ATTACH_FILES",
+              "SEND_MESSAGES",
+            ],
+            id: "886216752902586389",
+          },
+        ],
+      })
+      .then(async (msg: Message) => {
+        msg.channel.send(embed);
+        msg.channel.send(`<@${dados.id}>`).then(async (mencao: Message) => {
+          setTimeout(function () {
+            mencao.delete();
+          }, 1000);
         });
-    }
+      });
   }
 });
 
-client.on("message", (message) => {
-  if (message.channel.type == "dm") return;
+client.on("message", (message: Message) => {
+  console.log(`${message.author.username} sent a message: ${message.content}`);
+  if (message.channel.type === "dm") return;
   if (message.author.bot) return;
   if (!message.content.startsWith(config.prefix)) return;
 
@@ -105,8 +109,8 @@ client.on("message", (message) => {
   try {
     let commandFile = require(`./comandos/${command}.js`);
     commandFile.run(client, message, args);
-  } catch (err) {
-    if (err.code == "MODULE_NOT_FOUND") return;
+  } catch (err: any) {
+    if (err.code === "MODULE_NOT_FOUND") return;
     console.error(err);
   }
 });
